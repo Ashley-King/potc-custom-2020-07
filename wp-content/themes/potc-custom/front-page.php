11502cee7d86659f20e1939842d4a61b2e737c6e
Ashley King <ashleyot@gmail.com>
2020-07-19 15:20:24 -0400

    info-boxes on home

--- wp-content/themes/potc-custom/front-page.php
+++ wp-content/themes/potc-custom/front-page.php
@@ -1,8 +1,10 @@
 <?php
+$info = get_field('home_info');
+
 get_header();
 
 ?>
-<div class="home__hero">
+<section class="home__hero">
     
     <div class="home__hero__text container">
         <h1>It's time to focus on <span>your</span> potential for a change.</h1>
@@ -10,7 +12,23 @@ get_header();
         <div class="home__hero__cta"><a href="#">Join Us</a></div>
     </div>
     
-</div>
+</section>
+<section class="home__info">
+    <div class="home__info__boxes container ">
+    <?php foreach ($info as $box): ?>
+        <div class="home__info__boxes__box">
+            <div class="info-box__image">
+                <img src="<?php echo $box['icon']['url'] ?>" alt="icon">
+            </div>
+            <div class="info-box__text">
+                <?php echo $box['title'] ?>
+                <?php echo $box['text'] ?>
+            </div>
+        </div>
+    <?php endforeach?>
+
+    </div>
+</section>
 <?php
 
 get_footer();
